23e516e2d7a9b0b3769f1f203653c5d6e580db75
Vincenzo De Falco <vincenzodefalco89@gmail.com>
2025-11-11 05:59:22 -0500

    Update a Product implemented + Test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -179,6 +179,24 @@ class TestProductRoutes(TestCase):
         response = self.client.get(f"{BASE_URL}/{1234}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    # ----------------------------------------------------------
+    # TEST UPDATE
+    # ----------------------------------------------------------
+    def test_update_a_product(self):
+        """It should update a single Product"""
+        # Create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Update the product
+        new_product = response.get_json()
+        new_product["description"] = "test update"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product['description'], 'test update')
+
     ######################################################################
     # Utility functions
     ######################################################################
