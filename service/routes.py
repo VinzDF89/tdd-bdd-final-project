c175a6828d96b9885a68b687a805741511927b4b
Vincenzo De Falco <vincenzodefalco89@gmail.com>
2025-11-11 08:30:13 -0500

    List products by name + Test case implemented

--- service/routes.py
+++ service/routes.py
@@ -96,12 +96,17 @@ def create_products():
 
 @app.route("/products", methods=["GET"])
 def list_products():
-    """Get the Product list"""
+    """Returns a list of Products"""
     app.logger.info("Request to list Products...")
-
-    products = Product.all()
+    products = []
+    name = request.args.get("name")
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
     results = [product.serialize() for product in products]
-
     app.logger.info("[%s] Products returned", len(results))
     return results, status.HTTP_200_OK
 
